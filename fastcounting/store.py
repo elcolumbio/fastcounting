07c42aaed59aeeb7673c3a536f6c81ced7766e4f
Florian Benkö <20401370+elcolumbio@users.noreply.github.com>
2020-04-15 17:35:34 +0200

    simpler and better mappings

--- fastcounting/store.py
+++ fastcounting/store.py
@@ -13,15 +13,9 @@ def first_walk(df, batchtext):
     First of two walks for splitted multirow transactions only the first row
     has general information, we run only over those here.
     """
-    batchID = r.incr('next_batchID')  # rollback and diff functionality
-    r.hmset(f'batchID:{batchID}',
-        {'text': batchtext,
-         'date': str(int(dt.datetime.today().timestamp()))})
     for i in df.index:
         # get unique id from database (threadsave)
         generalID = r.incr('next_generalID')
-        # create mapping for rollback if we only run first_walk
-        r.sadd(f'batch:general:{batchID}', generalID)
         # create temporary mapping
         r.set(df.at[i, 'Nr.'], generalID, ex=300)
         # store data in hash
@@ -33,28 +27,29 @@ def first_walk(df, batchtext):
             'belegnr': df.at[i, 'Belegnr.']})
 
         
-def atomic_to_redis(i, konto, betrag, kontenseite, date, text, nr, ust=None):
+def atomic_to_redis(i, account, amount, kontenseite, date, text, nr, batch, ust=None):
     # get unique id from database (threadsave)
     atomicID = r.incr('next_atomicID')
     # get temporary mapping we created in the first walk
     generalID = r.get(nr)
-    batchID = r.get('next_batchID')
-    # create a lookup set for all atomis in a batch
-    r.sadd(f'batch:atomic:{batchID}', atomicID)
+
+    # batchfilter for diff and rollback
+    r.zadd('batch:atomic', {atomicID: batch})
+
     # create stable mapping- general:atomic:
-    r.sadd(f'general:atomic:{generalID}', atomicID)
+    r.zadd('general:atomic', {atomicID: generalID})
     # create mapping accountID:atomicID
-    r.sadd(f'account:atomic:{konto}', atomicID)
+    r.sadd('account:atomic', {atomicID: account})
     # create datefilter atomic:date
     r.zadd('atomic:date', {atomicID: date}) # could think about splitting the key into years
     # store data in hash + mapping atomic:general + mapping atomic+account
     r.hmset(f'atomicID:{atomicID}',
            {'generalID': generalID,
-            'accountID': konto,
+            'accountID': account,
             'text': text,
-            'amount': betrag,
+            'amount': amount,
             'kontenseite': kontenseite,
-            'batchID': batchID})
+            'batchID': batch})
         
 def second_walk(df):
     """
@@ -68,6 +63,8 @@ def second_walk(df):
     + every combination from the above
     It comes handy that split multirow transaction are seperated in rows.
     """
+    # this is not threadsave but writing to database is time intense. This should be fine.
+    batch = str(int(dt.datetime.today().timestamp()))
     for i in df.index:
         # date and nr are more like general fields, but text is different for split multirow.
         # for easy acces we store them in the atomic hash
@@ -75,16 +72,16 @@ def second_walk(df):
         text = df.at[i, 'Buchungstext']
         nr = df.at[i, 'Nr.']
         if df.at[i, 'Sollkto']:
-            atomic_to_redis(i, df.at[i, 'Sollkto'], -df.at[i, 'SollEUR'], 'Soll', date, text, nr)
+            atomic_to_redis(i, df.at[i, 'Sollkto'], -df.at[i, 'SollEUR'], 'Soll', date, text, nr, batch)
 
         if df.at[i, 'Habenkto']:
-            atomic_to_redis(i, df.at[i, 'Habenkto'], df.at[i, 'HabenEUR'], 'Haben', date, text, nr)
+            atomic_to_redis(i, df.at[i, 'Habenkto'], df.at[i, 'HabenEUR'], 'Haben', date, text, nr, batch)
 
         if df.at[i, 'USt Kto-H']:
-            atomic_to_redis(i, df.at[i, 'USt Kto-H'], df.at[i, 'USt H-EUR'], 'Haben', date, text, nr)
+            atomic_to_redis(i, df.at[i, 'USt Kto-H'], df.at[i, 'USt H-EUR'], 'Haben', date, text, nr, batch)
 
         if df.at[i, 'USt Kto-S']:
-            atomic_to_redis(i, df.at[i, 'USt Kto-S'], -df.at[i, 'USt-S EUR'], 'Soll', date, text,nr)
+            atomic_to_redis(i, df.at[i, 'USt Kto-S'], -df.at[i, 'USt-S EUR'], 'Soll', date, text,nr, batch)
 
 
 def main(month):
--- fastcounting/system.py
+++ fastcounting/system.py
@@ -10,7 +10,7 @@ r = redis.Redis(**helper.Helper().rediscred, decode_responses=True)
 
 def accountsystem_toredis(filename='kontenrahmen.txt'):
     """Push hash. E.g.: r.hgetall(f'accountsystem:{100}')."""
-    file = helper.Helper().datafolder('kontenrahmen.txt')
+    file = helper.Helper().datafolder(filename)
     system = pd.read_table(file, sep='\t', engine='python', decimal=',')
     for row in system.iterrows():  # iterrow for 1000 rows is ok
         data = dict(row[1])  # shortcut we are not explicit
