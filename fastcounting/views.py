b1466ba2f85730419ecee0ed57344c82adadb9c1
Florian Benkö <20401370+elcolumbio@users.noreply.github.com>
2020-04-20 15:14:05 +0200

    account view is now uptodate to

--- fastcounting/views.py
+++ fastcounting/views.py
@@ -42,7 +42,28 @@ for i, atomicID in pairs(redis.call(
     local system = redis.call('HGETALL', 'accountsystem:' .. atomic['accountID'])['map']
     if next(system)==nil then
         system = redis.call('HGETALL', 'accountsystem:special_account')['map'] end
-    
+
+
+    local inwords = {}
+    local haben = {}
+    for i, atomicx in pairs(redis.call(
+        'ZRANGEBYSCORE', 'general:atomic', atomic['generalID'], atomic['generalID'])) do
+        local inatomic = redis.call('HGETALL', 'atomicID:' .. atomicx)['map']
+        local part = inatomic['accountID'] .. ': ' .. inatomic['amount']/100 .. '€'
+        if inatomic['kontenseite'] == 'Soll' then
+            inwords[#inwords+1] = part
+        else
+            haben[#haben+1] = part
+        end
+    end
+
+    haben[#haben] = '### ' .. haben[#haben]
+    for i=1, #haben do
+        inwords[#inwords+1] = haben[i]
+    end
+    local relation = table.concat(inwords, ', ')
+
+
     redis.call('XADD','account:' .. atomic['accountID'],
         general['date'] .. '-' .. i, 
         'general', atomic['generalID'],
@@ -57,7 +78,8 @@ for i, atomicID in pairs(redis.call(
         'account_name', system['Kontenbezeichnung'],
         'system_kat', system['Kontenkategorie'],
         'system_type', system['Kontenunterart'],
-        'system_tax', system['Steuer']
+        'system_tax', system['Steuer'],
+        'relations', relation
         )
 end
 return true
@@ -96,7 +118,7 @@ for i, atomicID in pairs(redis.call(
     for i, atomicx in pairs(redis.call(
         'ZRANGEBYSCORE', 'general:atomic', atomic['generalID'], atomic['generalID'])) do
         local inatomic = redis.call('HGETALL', 'atomicID:' .. atomicx)['map']
-        local part = inatomic['accountID'] .. ':' .. inatomic['amount']/100 .. '€'
+        local part = inatomic['accountID'] .. ': ' .. inatomic['amount']/100 .. '€'
         if inatomic['kontenseite'] == 'Soll' then
             inwords[#inwords+1] = part
         else
